c8d128ae0af8fbb7fc8168b26247f64b150d820c
PGA68 <agdars@gmail.com>
2023-05-10 06:03:19 +0100

    Set normalized Base Date

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,13 +5,18 @@ import { useState, useEffect } from 'react'
 import { Select, Space } from 'antd'
 import { fetchAPI } from '@lib/fetchApi'
 import { useEffectOnce } from '@lib/useEffectOnce'
-// import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
-
+// import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
 
 function Main() {
-  const [dataChart, setDataChart] = useState([
+
+  const date2string = date => date.toLocaleString("ru", { month: "long", year: "numeric" })
+  const date2hash = date => Number(`${date.getFullYear()}${date.getMonth() + 11}${date.getDate() + 10}`)
+  const filterValue = localStorage.getItem("filterValue") || 'All'
+
+  const [filterCase, setFilterCase] = useState()
+  const [baseDate, setBaseDate] = useState()
+  const [dataChart0, setDataChart0] = useState([
     {
       name: 'Page A',
       uv: 4000,
@@ -56,15 +61,63 @@ function Main() {
     },
   ])
 
-  useEffect(() => {
+  const [dataChart, setDataChart] = useState([
+    {
+      date: 'Март 2004',
+      factory_id_1: 200,
+      factory_id_2: 400
+    },
+    {
+      date: 'Апрель 2004',
+      factory_id_1: 400,
+      factory_id_2: 200
+    },
+    {
+      date: 'Май 2004',
+      factory_id_1: 100,
+      factory_id_2: 100
+    },
+  ])
+
+  useEffectOnce(() => {
     fetchAPI(import.meta.env.VITE_API_HOST, { headers: { "Content-Type": "application/json" } })
-      .then(resume => {
-        console.log('Status = %s \nStatusText = %s \nJsonData = %o', resume.status, resume.statusText, resume.jsonData)
-        // setDataChart(resume.jsonData)
+      .then(({ status, statusText, jsonData }) => {
+        console.log('Status = %s \tStatusText = %s \nJsonData = %o', status, statusText, jsonData)
+
+        let acc = jsonData.reduce(
+          (a, b) => {
+
+            const factory = ['factory_1', 'factory_2'][b.factory_id - 1]
+            const milliSec = new Date(b.date)
+            const hashDate = date2hash(milliSec)
+
+            a[hashDate] ??= Object.create({
+              localeDate: date2string(milliSec),
+              factory_1: {
+                product_1: 0,
+                product_2: 0,
+                product_3: 0
+              },
+              factory_2: {
+                product_1: 0,
+                product_2: 0,
+                product_3: 0
+              }
+            })
+
+            if (b.product1) a[hashDate][factory].product_1 += b.product1
+            if (b.product2) a[hashDate][factory].product_2 += b.product2
+            if (b.product3) a[hashDate][factory].product_3 += b.product3
+            // console.log('a = %o\nb = %o', a, b)
+            return a
+          }, {},
+        )
+        console.log('acc = ', acc)
+        setBaseDate(acc)
       })
 
 
-    console.log(import.meta.env)
+    console.log('import.meta.env = ', import.meta.env)
     console.log('my effectOnce is running');
     return () => console.log('my effectOnce is destroying');
   }, []);
@@ -90,32 +143,62 @@ function Main() {
         />
       </Space>
       <h1>Bare Chart</h1>
-      <div className="card" style={{'width':'800px', 'height':'700px'}}>
-      <ResponsiveContainer width="100%" height="100%">
-        <BarChart
-          width={500}
-          height={300}
-          data={dataChart}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="pv" fill="#8884d8" />
-          <Bar dataKey="uv" fill="#82ca9d" />
-        </BarChart>
-      </ResponsiveContainer>
-        <p>
-          Mode is {currentMode}
-        </p>
+      <div className="card" style={{ width: '800px', height: '700px' }}>
+
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            width={500}
+            height={300}
+            data={dataChart0}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="pv" fill="#8884d8" />
+            <Bar dataKey="uv" fill="#82ca9d" />
+          </BarChart>
+        </ResponsiveContainer>
+
       </div>
+
+      <div className="card" style={{ width: '800px', height: '700px' }}>
+
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            width={500}
+            height={300}
+            data={dataChart}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="factory_id_1" fill="#8884d8" />
+            <Bar dataKey="factory_id_2" fill="#82ca9d" name="ываыва" textAnchor='Фабрика 2' label="d" textRendering="r" />
+          </BarChart>
+        </ResponsiveContainer>
+
+      </div>
+
+      <p>
+        Mode is {currentMode}
+      </p>
+
     </>
   )
 }
